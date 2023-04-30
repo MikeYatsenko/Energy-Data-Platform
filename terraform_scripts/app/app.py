ef4d94dbffc3e0414387cb3b963f167df93fb63f
MikeYatsenko <54863750+MikeYatsenko@users.noreply.github.com>
2023-04-30 12:49:12 +0200

    Update app.py

--- terraform_scripts/app/app.py
+++ terraform_scripts/app/app.py
@@ -6,8 +6,8 @@ import datetime
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource(service_name='s3', region_name='us-east-1', aws_access_key_id="AKIAUNZCPOZY2JEV4FRE",
-                        aws_secret_access_key="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")
+    s3 = boto3.resource(service_name='s3', region_name='us-east-1', aws_access_key_id=,
+                        aws_secret_access_key=)
 
     bucket_name = 'nuc-s3-bucket'
 
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
     df.drop('Unnamed: 0', axis=1, inplace=True)
-    engine = create_engine('postgresql://mikeyatsenko:yatsenkomike31@terraform-20230413194615999500000001'
+    engine = create_engine('postgresql://@terraform-20230413194615999500000001'
                            '.c0yflfvvzy5u.us-west-1.rds.amazonaws.com/postgres')
     df.to_sql('nuclear_data', engine, if_exists='append')
     return 'finished'
